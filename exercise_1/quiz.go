f289b07f471c5c0d0c542711f3e3c665e39e163a
Cyrus Ramavarapu <tribunus.vii@gmail.com>
2020-07-22 17:32:43 -0400

    Quiz game sans user input

--- exercise_1/quiz.go
+++ exercise_1/quiz.go
@@ -1,3 +1,4 @@
+// Quiz program based on input csv file
 package main
 
 import (
@@ -5,31 +6,63 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func main() {
+// Asks a question with an integer  answer
+// If the user answers correctly, true is returned.
+// Otherwise the function returns false
+func askQuestion(question *string, answer int) bool {
+	fmt.Printf("%s\n", *question)
+	//TODO(teirm) how do I take user input in again?
+	return true
+}
 
+func main() {
+	// allow the user to specify the file name for the quiz
 	var fileName = flag.String("filename", "", "File name for quiz")
 	flag.Parse()
 
+	// report an error if no quiz file is specified
 	if *fileName == "" {
 		fmt.Fprintf(os.Stderr, "usage: ./quiz -filename=<filename>\n")
 		os.Exit(1)
 	}
 
+	// open the file name specified
 	file, err := os.Open(*fileName)
 	if err != nil {
 		fmt.Print(err)
 	}
 
+	// read the file in as a default csv file
 	csvReader := csv.NewReader(file)
 	records, read_err := csvReader.ReadAll()
 	if read_err != nil {
 		fmt.Print(read_err)
 	}
 
+	// print out each record
+	var isCorrect bool
+	var correct int
+	var incorrect int
 	for _, record := range records {
-		fmt.Println(record)
+		if len(record) != 2 {
+			fmt.Fprintf(os.Stderr, "Bad question: %v\n", record)
+			continue
+		}
+		question := &record[0]
+		answer, err := strconv.Atoi(record[1])
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+		isCorrect = askQuestion(question, answer)
+		if isCorrect {
+			correct++
+		} else {
+			incorrect++
+		}
 	}
-
+	fmt.Printf("Quiz results: %v correct %v incorrect\n", correct, incorrect)
 }
