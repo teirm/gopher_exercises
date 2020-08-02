8b0f38d73f94dfd2859ffc6f5207937d6b8e28d7
Cyrus Ramavarapu <tribunus.vii@gmail.com>
2020-08-02 16:39:44 -0400

    Implemented JSON handler and file input

--- exercise_2/urlshort/main/main.go
+++ exercise_2/urlshort/main/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/teirm/gopher_exercises/exercise_2/urlshort"
 )
 
+// Read a file specified by the given path
+func readFile(filePath string) ([]byte, error) {
+	return ioutil.ReadFile(filePath)
+}
+
 func main() {
+
+	jsonFile := flag.String("json-file", "", "name of json file")
+	yamlFile := flag.String("yaml-file", "", "name of yaml file")
+	flag.Parse()
+
+	if *jsonFile != "" && *yamlFile != "" {
+		log.Fatal("Too many input. Specify either jsonFile or yamlFile")
+	}
+
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -17,20 +34,33 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
+	var handler http.HandlerFunc
+	if *jsonFile != "" {
+		jsn, err := readFile(*jsonFile)
+		if err != nil {
+			log.Fatal("Unable to read json file '%s': %v\n", *jsonFile, err)
+		}
+		handler, err = urlshort.JSONHandler(jsn, mapHandler)
+		if err != nil {
+			log.Fatal("Unable to create JSONHandler: %v\n", err)
+		}
+	} else if *yamlFile != "" {
+		yml, err := readFile(*yamlFile)
+		if err != nil {
+			log.Fatal("Unable to read yaml file '%s': %v\n", err)
+		}
+		handler, err = urlshort.YAMLHandler(yml, mapHandler)
+		if err != nil {
+			log.Fatal("Unable to create YAMLHandler: %v\n", err)
+		}
+	} else {
+		handler = mapHandler
+	}
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort  
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution 
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	err := http.ListenAndServe(":8080", handler)
+	fmt.Println("Exiting server: %v\n", err)
 }
 
 func defaultMux() *http.ServeMux {
